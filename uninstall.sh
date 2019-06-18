0e61e05fb5c022197d54b688fc469a0332cfa04a
Gilles Biagomba <gbiago961@gmail.com>
2019-06-18 13:46:47 -0400

    Updated uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -10,7 +10,6 @@ fi
 # Removing the Sherlock project file and soft link
 rm /usr/bin/sherlock
 rm /opt/Sherlock -rf
-rm /usr/bin/sherlock
 
 # Removing the jexboss dependency
 pip uninstall -r /opt/jexboss/requires.txt
@@ -21,6 +20,13 @@ pip3 uninstall -r /opt/XSStrike/requirements.txt
 rm /opt/XSStrike/ -rf
 rm /usr/bin/xsstrike
 
+# Removing the SpiderLabs Nmap Script dependency
+rm /opt/SpiderLabs/Nmap-Tools -rf
+rm /usr/share/nmap/scripts/http-screenshot.nse
+
+# Downloading the Vulners Nmap Script
+rm /opt/vulnersCom/nmap-vulners -rf
+rm /usr/share/nmap/scripts/nmap-vulners/vulners.nse
 
 # Removing remaining dependencies
 apt remove halberd sublist3r theharvester metagoofil nikto dirb nmap sn1pe masscan arachni sslscan testssl jexboss grabber golismero docker -y
