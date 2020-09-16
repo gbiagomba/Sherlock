9220c64c7f8f94f645910c881c9783279b7ec4c6
Mad Gray Hatter <gbiagomba@users.noreply.github.com>
2020-09-16 14:37:48 -0400

    Troubleshooting install script

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 # Checking dependencies - halberd, sublist3r, theharvester, metagoofil, nikto, dirb, masscan, nmap, sn1per, 
 #                         wapiti, sslscan, testssl, jexboss, xsstrike, grabber, golismero, docker, wappalyzer
 #                         sshscan, ssh-audit, dnsrecon, retirejs, python3, gobuster, seclists, metasploit
+set -eux
+trap "echo Booh!" SIGINT SIGTERM
+
 
 # Setting up variables
 OS_CHK=$(cat /etc/os-release | grep -o debian)
